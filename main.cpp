0eece71b4cb26dacb4a8f3266742f165713a91f1
matty45 <matthewtoo5@googlemail.com>
2020-08-19 03:14:17 +0100

    Revert "initial directx hook stuff"
    This reverts commit e7774f8d6cb2dbf538255d44943fd473d183ef6b.

--- main.cpp
+++ main.cpp
@@ -1,101 +1,46 @@
-// Example for D3D9 hook
+#include <iostream>
+#include <string>
+#include <windows.h>
 
-// Include required libraries
-#define MAX_VERTEX_BUFFER 512 * 1024
-#define MAX_INDEX_BUFFER 128 * 1024
-#define NK_INCLUDE_FIXED_TYPES
-#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
-#define NK_INCLUDE_FONT_BAKING
-#define NK_INCLUDE_DEFAULT_FONT
-#define NK_IMPLEMENTATION
-#define NK_D3D11_IMPLEMENTATION
-#include "dependencies/Nuklear/nuklear.h"
-#include "dependencies/Nuklear\demo\d3d11\nuklear_d3d11.h"
-#include "dependencies/kiero.h"
-#include <d3d11.h>
-#include <dxgi.h>
-#include <Windows.h>
+#pragma warning(disable:4996)
 
-// Initialize Variables
-HWND gamewindow = GetActiveWindow();
-WNDPROC oWndProc;
-ID3D11Device* pDevice = NULL;
-ID3D11DeviceContext* pContext = NULL;
-ID3D11RenderTargetView* mainRenderTargetView;
-nk_context* g_pNkContext;
-int WindowHeight = 0;
-int WindowWidth = 0;
+static void (*SendCommandToConsole)(int a1, char* cvar) = (void(__cdecl*)(int, char*))0x14059A050;
+std::string command;
 
-typedef HRESULT(__stdcall* Present) (IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags);
-Present oPresent;
+DWORD WINAPI dwConsole(LPVOID)
 
-void DoWindowValidation(void)
 {
-	RECT WindowRect;
-
-	if (GetClientRect(gamewindow, &WindowRect))
-	{
-		if (WindowHeight == 0)
-			WindowHeight = WindowRect.bottom - WindowRect.top;
-
-		if (WindowWidth == 0)
-			WindowWidth = WindowRect.right - WindowRect.left;
-
-		int CurrentWidth = WindowRect.right - WindowRect.left;
-		int CurrentHeight = WindowRect.bottom - WindowRect.top;
-
-		if (WindowHeight != CurrentHeight || WindowWidth != CurrentWidth)
-			GUIResize();
-	}
-}
-
-// Declare the detour function
-bool init = false;
-HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
-{
-	DoWindowValidation();
-
-	if (!init)
-	{
-		init = Initialize();
-	}
-	else
-	{
-		Start();
-
-		MenuBackend();
-
-		End();
-	}
+    //Initialize Console
+    AllocConsole();
+    AttachConsole(GetCurrentProcessId());
 	
-	return oPresent(pSwapChain, SyncInterval, Flags);
-}
+	//Redirect output to console
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
 
-DWORD WINAPI MainThread(LPVOID lpReserved)
-{
-	bool init_hook = false;
-	do
-	{
-		if (kiero::init(kiero::RenderType::D3D11) == kiero::Status::Success)
-		{
-			kiero::bind(8, (void**)&oPresent, hkPresent);
-			init_hook = true;
-		}
-	} while (!init_hook);
-	return TRUE;
-}
+    SetConsoleTitle(TEXT("Modern Warfare 2 Remastered Campaign - Dev Console"));
 
-BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
-{
-	switch (dwReason)
+	while (true)
 	{
-	case DLL_PROCESS_ATTACH:
-		DisableThreadLibraryCalls(hMod);
-		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
-		break;
-	case DLL_PROCESS_DETACH:
-		kiero::shutdown();
-		break;
+        // Get user input
+        std::cout << "\n> ";
+        std::getline(std::cin, command);
+        std::cout << command;
+
+		//(char*)"unbindall";;
+        // Send the command
+        SendCommandToConsole(0, _strdup(command.c_str()));
 	}
-	return TRUE;
+
+    return 0;
+}
+
+bool WINAPI DllMain(HMODULE hDll, DWORD dwReason, LPVOID lpReserved)
+{
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        CreateThread(0, 0, dwConsole, 0, 0, 0);
+        return TRUE;
+    }
+    return FALSE;
 }
